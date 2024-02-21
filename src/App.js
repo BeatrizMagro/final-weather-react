a9b95792b01b9b55bd54affb5f833e6ccf064b31
BeatrizMagro <127793547+BeatrizMagro@users.noreply.github.com>
2024-02-21 21:52:47 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Beatriz Magro and is{" "}
-        <a
-          href="https://github.com/BeatrizMagro/final-weather-react"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Beatriz Magro and is{" "}
+          <a
+            href="https://github.com/BeatrizMagro/final-weather-react"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

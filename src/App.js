d4e44327ae8919bbfce245f607e5c40ddfdd5d5d
BeatrizMagro <127793547+BeatrizMagro@users.noreply.github.com>
2024-02-21 21:23:42 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by Beatriz Magro and is{" "}
+        <a
+          href="https://github.com/BeatrizMagro/final-weather-react"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
